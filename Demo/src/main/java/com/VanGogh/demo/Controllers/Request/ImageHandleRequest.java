0f938923d20083b05e46aecdd769c21390358100
ranchen2003 <3081409323@qq.com>
2023-09-14 21:57:52 +0800

    基本完成2

---
+++ Demo/src/main/java/com/VanGogh/demo/Controllers/Request/ImageHandleRequest.java
@@ -0,0 +1,10 @@
+package com.VanGogh.demo.Controllers.Request;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class ImageHandleRequest {
+    private String image_url;
+}
---
+++ Demo/src/main/java/com/VanGogh/demo/Controllers/Request/LogoutRequest.java
@@ -0,0 +1,11 @@
+package com.VanGogh.demo.Controllers.Request;
+
+import lombok.Data;
+
+@Data
+public class LogoutRequest {
+    /**
+     * 用户名
+     */
+    private String userName;
+}
